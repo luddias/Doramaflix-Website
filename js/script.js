889788cf329bed691167a628c5649eb2d919dde4
Ludmila Dias <92104501+luddias@users.noreply.github.com>
2022-09-04 02:28:31 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+fundo=["bp", "ey", "bandc", "obs", "trw", "w"];
+lista=['drama-business','drama-extra','drama-badandcrazy','drama-ourbs','drama-trw','drama-w'];
+let i=0;
+function mudardrama(){  
+ 
+  let atual = document.getElementById(lista[i]);
+  let atual2 = document.getElementById(fundo[i]);
+
+  if (i==5){
+    i=-1;
+  }
+  let prox = document.getElementById(lista[i+1]);
+  atual.classList.remove('selecionado');
+  prox.classList.add('selecionado');
+
+  let prox2 = document.getElementById(fundo[i+1]);
+  atual2.classList.remove('fundo-selecionado');
+  prox2.classList.add('fundo-selecionado');
+  i++;
+}
+
+function voltardrama(){  
+ 
+  let atual = document.getElementById(lista[i]);
+  let atual2 = document.getElementById(fundo[i]);
+
+  if (i==0){
+    i=6;
+  }
+  let prox = document.getElementById(lista[i-1]);
+  atual.classList.remove('selecionado');
+  prox.classList.add('selecionado');
+
+  let prox2 = document.getElementById(fundo[i-1]);
+  atual2.classList.remove('fundo-selecionado');
+  prox2.classList.add('fundo-selecionado');
+
+  i--;
+}
